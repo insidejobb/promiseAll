2ca8c2ce3437cc1e00d7608744d85c43c1abde3e
insidejobb <117105647+insidejobb@users.noreply.github.com>
2023-03-15 19:30:45 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,6 +1,89 @@
-const promise1=Promise.resolve("helloWorld");
-const promise2=10;
-const promis3=new Promise((resolve,reject)=>
-    setTimeout(resolve,2000,'GOODBYE')
-);
-Promise.all([promise1,promise2,promis3]).then(values=>console.log(values))
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+
+</head>
+
+<body>
+
+    <h1>HELLOW WOrld</h1>
+
+    <script src="promise.js"></script>
+
+</body>
+
+</html>
+
+
+
+//javascript code to make the promises
+
+const posts = [
+    {name:"post one"},
+    {name:"post two"},
+    {name:"post three"},
+    {name:"post four"}
+]
+
+
+function createPost(post){
+    return new Promise((resolve,reject) => {
+        posts.push(post)
+        resolve(post);
+    })
+}
+
+
+function updateUserActivity (){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+
+            let lastActivity = new Date()
+            console.log(`Last seen : ${lastActivity}`)
+            resolve()
+
+        },1000)
+    })
+}
+
+
+function printPost(){
+    posts.forEach((post) => {
+        console.log(post.name)
+    })
+}
+
+
+function deletePost(){
+    return new Promise((resolve,reject) => {
+        if(posts.length > 0){
+
+            const del = posts.pop()
+            resolve(del)
+
+        }
+        else{
+            reject("ERROR")
+        }
+    })
+}
+
+
+
+
+
+Promise.all([createPost({name:"post five"}),updateUserActivity()])
+0 comments on commit f1ebc2c
+@insidejobb
+ 
+Add heading textAdd bold text, <Ctrl+b>Add italic 
